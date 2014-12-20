a9f3f6e12f615ca52fc5837f1d2600713fe51ec3
unknown <taojiang0101@gmail.com>
2014-12-21 00:58:13 +0800

    rm hello.c

--- test.c
+++ test.c
@@ -21,52 +21,22 @@
 #include<stdlib.h>
 #include<string.h>
 #include"public.h"
-#define MAX_NAME_SIZE 10
-#define MAX_DESCRIPTION_SIZE 10
 
-typedef union tagACL
+#pragma pack(1)
+struct tagACl
 {
-    UINT uiAclIndex;
-    CHAR szAclName[MAX_NAME_SIZE + 1];
-}ACL_GROUPINDEX_U;
-
-typedef struct tagACl
-{
-    CHAR szDescription[MAX_DESCRIPTION_SIZE + 1];
-    ACL_GROUPINDEX_U unAcl;
-    #define unAclName unAcl.szAclName
-    #define unAclIndex unAcl.uiAclIndex
-}ACL_S;
-
-STATIC VOID acl_Init(ACL_S *pstACL)
-{
-    strlcpy(pstACL->szDescription, "Hello!", sizeof(pstACL->szDescription));                
-    strlcpy(pstACL->unAclName, "VPN1", sizeof(pstACL->unAclName));
-
-    printf("ACL_1 Init:\n");
-    printf("\t%s\n", pstACL->szDescription);
-    printf("\t%s\n\n", pstACL->unAclName);
-}
+    UINT uiAclNum;
+    CHAR szAclName[10];
+};
+#pragma pack()
 
 INT main()
 {
-    ACL_S *pstACL1;
-    ACL_S *pstACL2;
+	struct tagAcl Acl1;
 
-    pstACL1 = (ACL_S *) calloc(1UL, sizeof(ACL_S));
-    
-    acl_Init(pstACL1);
-    
-    pstACL2 = (ACL_S *) calloc(1UL, sizeof(ACL_S));    
-    memcpy(pstACL2, pstACL1, sizeof(ACL_S));
+	Acl1.szAclName = 'hub1';
 
-    printf("ACL_1:\n");
-    printf("\t%s\n", pstACL1->szDescription);
-    printf("\t%s\n\n", pstACL1->unAclName);
+	printf("%s", Acl1.szAclName);
 
-    printf("ACL_2:\n");
-    printf("\t%s\n", pstACL2->szDescription);
-    printf("\t%s\n\n", pstACL2->unAclName);
-    
     return 0;
 }
